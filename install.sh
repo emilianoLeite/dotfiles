da10f4d6672a52dab914815f4c09cff7c4e92c2f
Emiliano Leite <emiliano.leite@codus.com.br>
2018-01-22 09:48:00 -0200

    Add installation script It install homebrew, zsh and oh-my-zsh

---
+++ install.sh
@@ -0,0 +1,12 @@
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install zsh
+brew install zsh zsh-completions
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# oh-my-zsh config
+cp ./.zshrc ~/
+cp emiliano.zsh-theme ~/.oh-my-zsh/themes/
